b18009943d7e0a9ce50c0ef0ee439beb3677f1ec
Ribha Mehrotra <ribha.mehrotra@gmail.com>
2015-12-13 22:13:31 +0530

    updated plot 3 and plot 4 for legend fix

--- plot3.R
+++ plot3.R
@@ -4,12 +4,13 @@ data <- read.csv.sql("household_power_consumption.txt", sql = "select * from fil
 data$Date <- as.Date(data$Date, format="%d/%m/%Y")
 data$datetime <- as.POSIXct(paste(as.Date(data$Date), data$Time))
 
+png(file = "plot3.png", height=480, width=480)
 plot(data$Sub_metering_1~data$datetime, type="l", ylab="Energy sub metering", xlab="")
 lines(data$Sub_metering_2~data$datetime,col='Red')
 lines(data$Sub_metering_3~data$datetime,col='Blue')
 
-legend("topright", col=c("black", "red", "blue"), 
-       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),lty=1, lwd=2, cex=0.8)
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       lty=1, lwd=par("lwd"), col=c("black", "red", "blue"))
 
-dev.copy(png, file="plot3.png", height=480, width=480)
+#dev.copy(png, file="plot3.png", height=480, width=480)
 dev.off()
